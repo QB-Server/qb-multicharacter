deb2899a375c738d55d4c327b7c6d2032d4c9131
Bobbins <bobbins@protonmail.com>
2022-04-14 22:07:37 +0100

    Changed coords

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {}
 Config.StartingApartment = false -- Enable/disable starting apartments (make sure to set default spawn coords)
-Config.Interior = vector3(-8.88, 512.91, 174.63) -- Interior to load where characters are previewed
+Config.Interior = vector3(-74.87, -821.8, 285.0) -- Interior to load where characters are previewed
 Config.DefaultSpawn = vector3(-1035.71, -2731.87, 12.86) -- Default spawn coords if you have start apartments disabled
-Config.PedCoords = vector4(152.92, -767.25, 257.74, 70.97) -- Create preview ped at these coordinates
-Config.HiddenCoords = vector4(-76.39, -818.76, 326.18, 68.65) -- Hides your actual ped Ma-cro guapo while you are in selection
-Config.CamCoords = vector4(152.31, -767.87, 257.78, 280.87) -- Camera coordinates for character preview screen 
+Config.PedCoords = vector4(-63.62, -821.69, 285.62, 124.97) -- Create preview ped at these coordinates
+Config.HiddenCoords = vector4(-82.47, -810.15, 285.0, 219.39) -- Hides your actual ped Ma-cro guapo while you are in selection
+Config.CamCoords = vector4(-66.09, -822.89, 285.61, 296.6) -- Camera coordinates for character preview screen 
